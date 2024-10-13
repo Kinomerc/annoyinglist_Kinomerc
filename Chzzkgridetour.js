3c19c29024de833bc9c6a6d1ff4cbd5be09499ff
Kinomerc <96902592+Kinomerc@users.noreply.github.com>
2024-10-13 19:42:30 +0900

    Create Chzzkgridetour.js

---
+++ Chzzkgridetour.js
@@ -0,0 +1,53 @@
+// ==UserScript==
+// @name         치지직 그리드 우회
+// @version      1.0
+// @description  치지직 그리드 우회 최신방식을 사용합니다
+// @author       Kinomerc
+// @match        *://chzzk.naver.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // 초기 유저 에이전트 설정
+    const initialUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36";
+    const newUserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36";
+
+    // 유저 에이전트 변경
+    Object.defineProperty(navigator, 'userAgent', {
+        get: () => initialUserAgent,
+    });
+
+    // 버튼을 자동으로 클릭하는 함수
+    function autoClickButton() {
+        const button = document.querySelector('button.button_container__x044H.button_largest__WpFk2.button_square__PyH4Y.button_highlight__K-Up9.button_full_button__F5MXC');
+        if (button) {
+            button.click();
+            console.log('버튼 클릭됨');
+            // 유저 에이전트를 새로운 값으로 변경
+            Object.defineProperty(navigator, 'userAgent', {
+                get: () => newUserAgent,
+            });
+        }
+    }
+
+    // DOM 변화 감지
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.addedNodes.length) {
+                const liveInfo = document.querySelector('.live_information_video_layer__Vd4JD');
+                if (liveInfo) {
+                    autoClickButton();
+                }
+            }
+        });
+    });
+
+    // 감지할 DOM 노드 설정
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
+
+})();
